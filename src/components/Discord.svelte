9b8393e8e028d3a5df2d20295e3c5c9fe4cd940c
Steven Carpenter <specCon18@gmail.com>
2023-01-31 00:07:32 -0800

    added discord widget started some mobile support work changed a few styles and setup svelte integration

---
+++ src/components/Discord.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+</script>
+
+<section class="hidden sm:block">
+    <h3 class="text-gray-50 py-5 text-xl">Wanna Chat? Join My Discord!</h3>
+    <iframe class="mx-auto" title=" " src="https://discord.com/widget?id=857047360551125013&theme=dark" width="350" height="500" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+</section>
+
+<style>
+
+</style>
