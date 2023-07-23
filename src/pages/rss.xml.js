e41e547ac5fc2f9f1538c2251a52c79ba9ceb9f5
specCon18 <steven.carpenter@skdevstudios.com>
2023-07-23 18:01:53 -0400

    Updated LICENSE

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,3 +1,21 @@
+// Steven Carpenter's Blog
+// Copyright (C) 2023  Steven Carpenter
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// If you have any questions about this license you may contact me @ steven.carpenter@skdevstudios.com
+
 import rss from "@astrojs/rss";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../config";
 
@@ -8,3 +26,5 @@ export const get = () =>
     site: import.meta.env.SITE,
     items: import.meta.glob("./blog/**/*.{md,mdx}"),
   });
+
+  
